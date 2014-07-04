56e3fdf553c37dd152788ea7074c9bd20d990dfa
Yuji Nakayama <nkymyj@gmail.com>
2014-07-04 22:19:19 +0900

    Bump version to 0.1.0

--- lib/asterisk/version.rb
+++ lib/asterisk/version.rb
@@ -5,8 +5,8 @@ module Asterisk
   # http://semver.org/
   module Version
     MAJOR = 0
-    MINOR = 0
-    PATCH = 1
+    MINOR = 1
+    PATCH = 0
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
