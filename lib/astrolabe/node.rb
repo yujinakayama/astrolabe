yujinakayama/astrolabe

commit 695e58d47b88858e07c5b0195659f64ae1f9a745

Enable filtering nodes by type in node enumeration method

lib/astrolabe/node.rb
@@ -69,13 +69,14 @@ module Astrolabe
     # @yieldparam [Node] node each ancestor node
     # @return [self] if a block is given
     # @return [Enumerator] if no block is given
-    def each_ancestor
+    def each_ancestor(*types)
       return to_enum(__method__) unless block_given?
 
+      types.flatten!
       last_node = self
 
       while (current_node = last_node.parent)
-        yield current_node
+        yield current_node if types.empty? || types.include?(current_node.type)
         last_node = current_node
       end
 
@@ -91,12 +92,14 @@ module Astrolabe
     # @yieldparam [Node] node each child node
     # @return [self] if a block is given
     # @return [Enumerator] if no block is given
-    def each_child_node
+    def each_child_node(*types)
       return to_enum(__method__) unless block_given?
+
+      types.flatten!
 
       children.each do |child|
         next unless child.is_a?(Node)
-        yield child
+        yield child if types.empty? || types.include?(child.type)
       end
 
       self
@@ -108,9 +111,10 @@ module Astrolabe
     # @yieldparam [Node] node each descendant node
     # @return [self] if a block is given
     # @return [Enumerator] if no block is given
-    def each_descendant(&block)
+    def each_descendant(*types, &block)
       return to_enum(__method__) unless block_given?
-      visit_descendants(&block)
+      types.flatten!
+      visit_descendants(types, &block)
       self
     end
 
@@ -123,19 +127,20 @@ module Astrolabe
     # @yieldparam [Node] node each node
     # @return [self] if a block is given
     # @return [Enumerator] if no block is given
-    def each_node(&block)
+    def each_node(*types, &block)
       return to_enum(__method__) unless block_given?
-      yield self
-      each_descendant(&block)
+      types.flatten!
+      yield self if types.empty? || types.include?(type)
+      each_descendant(*types, &block)
     end
 
     protected
 
-    def visit_descendants(&block)
+    def visit_descendants(types, &block)
       children.each do |child|
         next unless child.is_a?(Node)
-        yield child
-        child.visit_descendants(&block)
+        yield child if types.empty? || types.include?(child.type)
+        child.visit_descendants(types, &block)
       end
     end
   end

benchmark/performance_spec.rb
@@ -67,9 +67,10 @@ describe 'performance' do
           def each_descendant
             return to_enum(__method__) unless block_given?
 
-            each_child_node do |child_node|
-              yield child_node
-              child_node.each_descendant { |node| yield node }
+            children.each do |child|
+              next unless child.is_a?(self.class)
+              yield child
+              child.each_descendant { |node| yield node }
             end
           end
         end
@@ -82,9 +83,10 @@ describe 'performance' do
           def each_descendant(&block)
             return to_enum(__method__) unless block_given?
 
-            each_child_node do |child_node|
-              yield child_node
-              child_node.each_descendant(&block)
+            children.each do |child|
+              next unless child.is_a?(self.class)
+              yield child
+              child.each_descendant(&block)
             end
           end
         end
@@ -100,9 +102,10 @@ describe 'performance' do
 
             proc_object ||= block
 
-            each_child_node do |child_node|
-              proc_object.call(child_node)
-              child_node.each_descendant(proc_object)
+            children.each do |child|
+              next unless child.is_a?(self.class)
+              proc_object.call(child)
+              child.each_descendant(proc_object)
             end
           end
         end

spec/astrolabe/node_spec.rb
@@ -115,6 +115,42 @@ module Astrolabe
       end
 
       include_examples 'node enumerator', :each_ancestor
+
+      context 'when a node type symbol is passed' do
+        it 'scans all the ancestor nodes but yields only nodes matching the type' do
+          yielded_types = []
+
+          target_node.each_ancestor(:begin) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:begin])
+        end
+      end
+
+      context 'when multiple node type symbols are passed' do
+        it 'scans all the ancestor nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_ancestor(:begin, :def) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:def, :begin])
+        end
+      end
+
+      context 'when an array including type symbols are passed' do
+        it 'scans all the ancestor nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_ancestor([:begin, :def]) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:def, :begin])
+        end
+      end
     end
 
     describe '#each_child_node' do
@@ -151,6 +187,42 @@ module Astrolabe
       end
 
       include_examples 'node enumerator', :each_child_node
+
+      context 'when a node type symbol is passed' do
+        it 'scans all the child nodes but yields only nodes matching the type' do
+          yielded_types = []
+
+          target_node.each_child_node(:send) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:send])
+        end
+      end
+
+      context 'when multiple node type symbols are passed' do
+        it 'scans all the child nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_child_node(:send, :args) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:args, :send])
+        end
+      end
+
+      context 'when an array including type symbols are passed' do
+        it 'scans all the child nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_child_node([:send, :args]) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:args, :send])
+        end
+      end
     end
 
     describe '#each_descendant' do
@@ -196,6 +268,42 @@ module Astrolabe
       end
 
       include_examples 'node enumerator', :each_descendant
+
+      context 'when a node type symbol is passed' do
+        it 'scans all the descendant nodes but yields only nodes matching the type' do
+          yielded_types = []
+
+          target_node.each_descendant(:send) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:send, :send])
+        end
+      end
+
+      context 'when multiple node type symbols are passed' do
+        it 'scans all the descendant nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_descendant(:send, :def) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:send, :def, :send])
+        end
+      end
+
+      context 'when an array including type symbols are passed' do
+        it 'scans all the descendant nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_descendant([:send, :def]) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:send, :def, :send])
+        end
+      end
     end
 
     describe '#each_node' do
@@ -224,7 +332,7 @@ module Astrolabe
       let(:expected_types) { [:class, :const, :begin, :send, :sym, :def, :args, :arg, :arg, :send] }
 
       context 'when a block is given' do
-        it 'yields itself and each descendant node with depth first order' do
+        it 'yields the node itself and each descendant node with depth first order' do
           yielded_types = []
 
           target_node.each_node do |node|
@@ -241,6 +349,42 @@ module Astrolabe
       end
 
       include_examples 'node enumerator', :each_node
+
+      context 'when a node type symbol is passed' do
+        it 'scans all the nodes but yields only nodes matching the type' do
+          yielded_types = []
+
+          target_node.each_node(:send) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:send, :send])
+        end
+      end
+
+      context 'when multiple node type symbols are passed' do
+        it 'scans all the nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_node(:send, :def) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:send, :def, :send])
+        end
+      end
+
+      context 'when an array including type symbols are passed' do
+        it 'scans all the nodes but yields only nodes matching any of the types' do
+          yielded_types = []
+
+          target_node.each_node([:send, :def]) do |node|
+            yielded_types << node.type
+          end
+
+          expect(yielded_types).to eq([:send, :def, :send])
+        end
+      end
     end
 
     describe '#send_type?' do

commit 794a35cf5196444dc1f027de4ce61a00c029cf5a

Recover performance regression caused by 695e58d

lib/astrolabe/node.rb
@@ -81,15 +81,15 @@ module Astrolabe
     # @yieldparam [Node] node each ancestor node
     # @return [self] if a block is given
     # @return [Enumerator] if no block is given
-    def each_ancestor(*types)
+    def each_ancestor(*types, &block)
       return to_enum(__method__, *types) unless block_given?
 
       types.flatten!
-      last_node = self
 
-      while (current_node = last_node.parent)
-        yield current_node if types.empty? || types.include?(current_node.type)
-        last_node = current_node
+      if types.empty?
+        visit_ancestors(&block)
+      else
+        visit_ancestors_with_types(types, &block)
       end
 
       self
@@ -149,8 +149,15 @@ module Astrolabe
     # @return [Enumerator] if no block is given
     def each_descendant(*types, &block)
       return to_enum(__method__, *types) unless block_given?
+
       types.flatten!
-      visit_descendants(types, &block)
+
+      if types.empty?
+        visit_descendants(&block)
+      else
+        visit_descendants_with_types(types, &block)
+      end
+
       self
     end
 
@@ -177,19 +184,55 @@ module Astrolabe
     # @return [Enumerator] if no block is given
     def each_node(*types, &block)
       return to_enum(__method__, *types) unless block_given?
+
       types.flatten!
+
       yield self if types.empty? || types.include?(type)
-      visit_descendants(types, &block)
+
+      if types.empty?
+        visit_descendants(&block)
+      else
+        visit_descendants_with_types(types, &block)
+      end
+
       self
     end
 
     protected
 
-    def visit_descendants(types, &block)
+    def visit_descendants(&block)
       children.each do |child|
         next unless child.is_a?(Node)
-        yield child if types.empty? || types.include?(child.type)
-        child.visit_descendants(types, &block)
+        yield child
+        child.visit_descendants(&block)
+      end
+    end
+
+    def visit_descendants_with_types(types, &block)
+      children.each do |child|
+        next unless child.is_a?(Node)
+        yield child if types.include?(child.type)
+        child.visit_descendants_with_types(types, &block)
+      end
+    end
+
+    private
+
+    def visit_ancestors
+      last_node = self
+
+      while (current_node = last_node.parent)
+        yield current_node
+        last_node = current_node
+      end
+    end
+
+    def visit_ancestors_with_types(types)
+      last_node = self
+
+      while (current_node = last_node.parent)
+        yield current_node if types.include?(current_node.type)
+        last_node = current_node
       end
     end
   end