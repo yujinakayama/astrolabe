95520eef5d04ff382b21944692d5f082279e976f
Yuji Nakayama <nkymyj@gmail.com>
2014-07-18 15:12:10 +0900

    Bump version to 0.6.0

--- lib/astrolabe/version.rb
+++ lib/astrolabe/version.rb
@@ -5,8 +5,8 @@ module Astrolabe
   # http://semver.org/
   module Version
     MAJOR = 0
-    MINOR = 5
-    PATCH = 2
+    MINOR = 6
+    PATCH = 0
 
     def self.to_s
       [MAJOR, MINOR, PATCH].join('.')
