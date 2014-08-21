f2ca056b09bc35fc3907226bead02ac252ee7bcb
Yuji Nakayama <nkymyj@gmail.com>
2014-08-22 03:50:04 +0900

    Bump version to 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## Development
+
+## v1.0.0
+
+* Initial API stable release in the Semantic Versioning.
--- lib/astrolabe/version.rb
+++ lib/astrolabe/version.rb
@@ -4,8 +4,8 @@ module Astrolabe
   # @api private
   # http://semver.org/
   module Version
-    MAJOR = 0
-    MINOR = 6
+    MAJOR = 1
+    MINOR = 0
     PATCH = 0
 
     def self.to_s
